897b019902f297487c7db0e83219895a207c7d41
isaac <isaacdener21@gmail.com>
2023-04-19 00:20:38 -0600

    cambio ruta sw

--- sw.js
+++ sw.js
@@ -10,12 +10,11 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 
 const APP_SHELL = [
-    //'/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
     'img/avatars/hulk.jpg',
-    'img/avatars/ironman.jpg',
+    'img/avatars/ironman.jpg',   
     'img/avatars/spiderman.jpg',
     'img/avatars/thor.jpg',
     'img/avatars/wolverine.jpg',
